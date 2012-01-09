24efe9ba7b5e4242dc67de75cb091aa9cef729ae
Andrew Edgerton <edge87@gmail.com>
2012-01-09 10:49:02 -0600

    Sync From Laptop

---
+++ csharp/GradeBookTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class GradeBookTest{
+   static void Main(string[] args){
+      GradeBook myGradeBook = new GradeBook();
+
+      myGradeBook.DisplayMessage();
+      
+
+
+   }
+}
+
+public class GradeBook {
+   public void DisplayMessage(){
+      Console.WriteLine("Welcome to the Grade Book");
+    }
+}
+
+
+
+
