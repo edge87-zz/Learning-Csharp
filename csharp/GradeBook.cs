cc798f2b50915ee4444a63e54b0e1881090a4c4d
Andrew Edgerton <edge87@gmail.com>
2012-01-22 22:05:11 -0600

    page 104

--- csharp/GradeBook.cs
+++ csharp/GradeBook.cs
@@ -5,7 +5,20 @@ using System;
 
 
 public class GradeBook {
+   private string courseName;
+
+
+   public string CourseName {
+      get {
+         return courseName;
+      }
+
+      set {
+         courseName = value;
+      }
+   }
+
    public void DisplayMessage( string courseName) {
-      Console.WriteLine("Welcome to the grade book for:\n{0}!", courseName);
+      Console.WriteLine("Welcome to the grade book for:\n{0}!", CourseName);
    }
 }
