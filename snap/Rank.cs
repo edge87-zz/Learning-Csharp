754fa2bbae1d286e8008b219085711564af5df4d
Andrew Edwards <a@ajed.net>
2012-01-28 18:58:03 +0000

    Added files

---
+++ snap/Rank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snap
+{
+    class Rank
+    {
+        enum Rank { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
+    }
+}
