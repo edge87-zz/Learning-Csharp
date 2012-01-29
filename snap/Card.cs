99a50c85fef800b68da3a6396a1ccfd1ea4bd354
Andrew Edwards <a@ajed.net>
2012-01-29 09:15:21 +0000

    added comments to the classes for notes

--- snap/Card.cs
+++ snap/Card.cs
@@ -2,13 +2,18 @@
 {
     class Card
     {
+        // Variables of Rank and Suit type
         private Rank _rank;
         private Suit _suit;
+
+        //Constructor for a card object
         public Card(Rank rank, Suit suit)
         {
             _rank = rank;
             _suit = suit;
         }
+
+        // returns a string which says the value of the card
         public string GetValue()
         {
             return _rank + " of " + _suit;
